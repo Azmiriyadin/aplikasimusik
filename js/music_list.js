cf8e2207c5ef962eee134d19dedadf765a1d0d1a
Azmi Riyadin <Mzamzamiriyadin@gmail.com>
2023-11-28 11:50:00 +0700

    Aplikasi Musik

---
+++ js/music_list.js
@@ -0,0 +1,30 @@
+let musicList = [
+    {
+        name: "NEMU",
+        artist: "Gilga Sahid",
+        img: "img/gilgasahid.jpg",
+        src: "audio/nemu-gilgasahid.mp3",
+        love: false,
+    },
+    {
+        name: "KLEBUS",
+        artist: "Yeni Inka",
+        img: "img/yeniinka.jpg",
+        src: "audio/klebus-yeniinka.mp3",
+        love: false,
+    },
+    {
+        name: "TAMAN JURUG",
+        artist: "Deny Caknan",
+        img: "img/denycaknan.jpg",
+        src: "audio/tamanjurug-denycaknan.mp3",
+        love: false,
+    },
+    {
+        name: "SEMOGA KAU DI NERAKA",
+        artist: "Endank Soekamti",
+        img: "img/endanksoekamti.jpg",
+        src: "audio/semogakaudineraka-endank.mp3",
+        love: false
+    }
+];
